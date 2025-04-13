6316d280bbf9aac220a19f6d2cf620af1b17b16c
petxcode <brighterdayscodelab@gmail.com>
2025-04-13 09:21:08 +0100

    WIIP

--- src/pagesForTeachers/api/teachersAPI.ts
+++ src/pagesForTeachers/api/teachersAPI.ts
@@ -7,8 +7,6 @@ import axios from "axios";
 // const URL: string =
 //   import.meta.env.VITE_MAIN_URL || import.meta.env.VITE_PRODUCTION_URL;
 
-// const URL: string = "http://localhost:2244/api";
-
 const URL: string = "https://startschoolbe-4.onrender.com/api";
 
 // Working Online
